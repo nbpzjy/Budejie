b9d14c13542b94632667943cb9f9200736aa63bc
nbpzjy <zjygzc@gmail.com>
2016-06-25 22:48:43 +0800

    commplete GuideActivity jump to MainActivity

--- app/src/main/java/com/nbpzjy/dream_6_20_budejie/GuideActivity.java
+++ app/src/main/java/com/nbpzjy/dream_6_20_budejie/GuideActivity.java
@@ -1,6 +1,7 @@
 package com.nbpzjy.dream_6_20_budejie;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nbpzjy.dream_6_20_budejie.views.CircleIndicator;
 
@@ -73,6 +75,46 @@ public class GuideActivity extends Activity {
         vp_guide.setAdapter(guideAdapter);
 
         indicator.setViewPager(vp_guide);
+
+        //开始调用tv_gomain，用TextView定义一个tv_main取得布局文件中的参数,用final固定tv_main的值不变
+        final TextView tv_main = (TextView)findViewById(R.id.tv_main);
+        //开始编写跳转代码
+        //在这里设置监听点击，点击时从GuideActivity启动MainActivity
+        tv_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GuideActivity.this,MainActivity.class));
+
+            }
+        });
+
+        //实现跳转前，对vp_guide设置监听
+        //主要是在检测welcome页是否为最后一页
+        //如果是最后一页，则显示进入主页几个字，否则不显示
+        vp_guide.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //判断当前叶的position是不是整个guide页中的imageviewlist中的最后一个页面
+                // 方法就是整个imageviewlist-1，从第0个开始
+                if (position < imageViewList.size()-1){
+                    //如果不是最后一页，那么不显示tv_gomain
+                    tv_main.setVisibility(View.GONE);
+                    //如果是最后一页，那么显示tv_main用以跳转至mainactivity
+                }else {tv_main.setVisibility(View.VISIBLE);
+
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
     class GuideAdapter extends PagerAdapter{
